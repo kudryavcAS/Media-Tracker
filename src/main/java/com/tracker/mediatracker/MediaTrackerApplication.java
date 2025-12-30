7a12fdef7891da9c4287ebc545fded016f73bc95
Alexey Kudryavtsev <aleksey.kudryavtsew@gmail.com>
2025-12-30 14:02:44 +0300

    chore: initial project setup with Spring Boot and SQLite

---
+++ src/main/java/com/tracker/mediatracker/MediaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.mediatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/mediatracker/MediaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.mediatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
